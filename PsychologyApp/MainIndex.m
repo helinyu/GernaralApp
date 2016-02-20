c749d221826a1318bacbf8aeee40a7572aaf0de7
felix <2319979647@qq.com>
2016-02-20 16:17:04 +0800

    app con图标的添加

--- PsychologyApp/MainIndex.m
+++ PsychologyApp/MainIndex.m
@@ -16,19 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    self.navigationItem.title = @"舒心";    
+
+    
+    
     
 }
 
-
-
-
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 /*
